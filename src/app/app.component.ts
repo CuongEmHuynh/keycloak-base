909d6e7f5f90b637198370d3356b7c5926554fc5
“HuynhEmCuong” <huynhcuongem7597@gmail.com>
2025-03-27 22:08:02 +0700

    Revert "# commit angular"
    This reverts commit 9e875e7ca58cfdf68d98ca5f42ab78112716984c.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { KeycloakService } from 'keycloak-angular';
-import { KeycloakProfile } from 'keycloak-js';
+import { Component } from '@angular/core';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
-
-  public isLoggedIn = false;
-  public userProfile: KeycloakProfile | null = null;
-
-  constructor(private readonly keycloak: KeycloakService) { }
-
-  public async ngOnInit() {
-    this.isLoggedIn = await this.keycloak.isLoggedIn();
-
-    if (this.isLoggedIn) {
-      this.userProfile = await this.keycloak.loadUserProfile();
-      console.log(this.userProfile)
-    }
-  }
-
-  public login() {
-    this.keycloak.login();
-  }
-
-  public logout() {
-    this.keycloak.logout();
-  }
-
-  title: string = "Angular"
+export class AppComponent {
+  title = 'angular-keycloak';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,45 +1,18 @@
-import { APP_INITIALIZER, NgModule } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { KeycloakService } from 'keycloak-angular';
 
 @NgModule({
   declarations: [
-    AppComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-
+    AppRoutingModule
   ],
-  providers: [
-    KeycloakService,
-    {
-      provide: APP_INITIALIZER,
-      useFactory: initializeKeycloak,
-      multi: true,
-      deps: [KeycloakService]
-    }
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
-
-export function initializeKeycloak(keycloak: KeycloakService) {
-  return () =>
-    keycloak.init({
-      config: {
-        realm: 'master',
-        url: 'http://localhost:8080',
-        clientId: 'angular-app'
-      },
-      initOptions: {
-        onLoad: 'check-sso',
-        silentCheckSsoRedirectUri: window.location.origin + '/assets/silent-check-sso.html'
-      }
-    });
-}
-
-
